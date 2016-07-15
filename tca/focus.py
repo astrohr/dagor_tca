b9b9f5cea1a413ed888435d447195f01271c5611
frnhr <fran.hrzenjak@gmail.com>
2016-07-15 02:40:55 +0200

    CLI module - stub

--- tca/focus.py
+++ tca/focus.py
@@ -3,31 +3,38 @@
 
 Usage:
     focus.py status
-    focus.py step_tp <N>
-    focus.py reset
+    focus.py step to <N>
+    focus.py step by <N>
+    focus.py set <N>
     focus.py -h | --help
     focus.py --version
 
 Commands:
-    status         Display focuser status
-    step_to         Switch a particular switch on or off. Sepcify "all" to affect all available switches. (NOT IMPLEMENTED9
-    reset          Reset Arduino. (NOT IMPLEMENTED)
+    status          Display status
+    step to         Move to specified position
+    step by         Move by specified number of steps
+    set             Set current position
 
 Options:
-    -h --help      Show this screen or description of specific command.
-    --version      Show version.
+    -h --help       Show this screen or description of specific command.
+    --version       Show version.
 """
-  
+from pprint import pprint
+
 from docopt import docopt
 import serial
 import sys
-from time import sleep
 
-RESET_DISABLED = True
+from _utils import str_bool
+
+
+RESET_DISABLED = False
+
 SERIAL = {
-    'PORT' : '/dev/tty.usbserial-A703F1A4',
-    'BAUDRATE' : 115200,
-    'TIMEOUT' : 0.1,  # if no data available, block for max this many seconds
+    # 'PORT': '/dev/tty.usbserial-A703F1A4',  # Not!
+    'PORT': '/dev/cu.wchusbserialfa130',  # Mac
+    'BAUDRATE': 115200,
+    'TIMEOUT': 0.1,  # if no data available, block for max this many seconds
 }
 
 
@@ -37,22 +44,22 @@ class FocuserController(object):
         Note: it always requests latest status from Arduino controller.
         
      .. Serial settings: 
-        9600 baud, newline line ending
+        115200 baud, newline line ending
 
      .. Protocol:
-         .. Query:
-            Single line
-            Options: 
-             * status
-             * switch <N> (on|off)
-             * switch all (on|off)
-             * reset
+         TODO describe protocol
     """
 
     # public API
     @property
-    def n(self):
-        return self._n
+    def position(self):
+        self._refresh_status()
+        return self._status['position']
+
+    @property
+    def can_go_up(self):
+        self._refresh_status()
+        return self._status['can_go_up']
 
     @property
     def status(self):
@@ -68,9 +75,7 @@ class FocuserController(object):
 
     def pretty_status(self):
         self._refresh_status()
-        print('number of switches: {}'.format(self._n))
-        for i in range(self._n):
-            print("switch {}: {}".format(i + 1, "ON" if self._status[i] else "OFF"))
+        pprint(self._status)
 
     def switch(self, n, state):
         if n == 'all':
@@ -79,49 +84,91 @@ class FocuserController(object):
         else:
             self._switch(int(n), state)
 
-    # peivate members
+    # private members
 
-    _n  = 0  # number of swiotches available to this controller
-    _status = []  # state of each switch: True for ON, False for OFF 
+    _status = None
+    _default_status = {
+        'position': 0,
+        'idle': False,
+        'stopping_hard': False,
+        'moving_up': False,
+        'moving_dn': False,
+        'intent_up': False,
+        'intent_dn': False,
+        'can_go_up': False,
+        'can_go_dn': False,
+    }
+    _default_status_type = {
+        'position': int,
+        'idle': str_bool,
+        'stopping_hard': str_bool,
+        'moving_up': str_bool,
+        'moving_dn': str_bool,
+        'intent_up': str_bool,
+        'intent_dn': str_bool,
+        'can_go_up': str_bool,
+        'can_go_dn': str_bool,
+    }
     _serial = None  # serial.Serial()
 
     def _open_serial(self):
-        """ Open serial port to Arduino controller. Waits for acknowlegement (string "ready").
+        """ Open serial port to Arduino controller. Waits for acknowledgement (string "ready").
             Throws CommunicationException.
         """
-        self._serial = serial.Serial()
-        self._serial.port = self._SERIAL['PORT']
-        self._serial.baudrate = self._SERIAL['BAUDRATE']
-        self._serial.timeout = self._SERIAL['TIMEOUT']
-        self._serial.open()
-        while self._serial.read():
-            pass
+        self._serial = serial.Serial(
+            port=self._SERIAL['PORT'],
+            baudrate=self._SERIAL['BAUDRATE'],
+            timeout=self._SERIAL['TIMEOUT'],
+            dsrdtr=not RESET_DISABLED,
+            rtscts=not RESET_DISABLED,
+        )
+        # self._serial.open()
         if not self._RESET_DISABLED:
-            for i in range(6):
+            for i in range(60):  # TODO magic magic magic!
                 line = self._serial.readline().strip()
+                print("line: {}".format(line))
                 if line and line == 'ready':
-                    return  # TODO maybe wait for next readline to timeout, so we are sure this is the last data in serial pipe?
+                    pass
+                    #return  # TODO maybe wait for next readline to timeout, so we are sure this is the last data in serial pipe?
             raise CommunicationException('Cannot establish serial communication: Arduino not reporting ready')
         else:  # self._RESET_DISABLED
-            pass  # TOOD send a "ready?" query?
+            pass  # TODD send a "ready?" query?
 
     def _refresh_status(self):
+        # send command:
         self._serial.write('status\n')
-        response = self._serial.readline().strip()
-        if response != 'status':
-            raise CommunicationException('Controller doesnt acknowlege status command, instead got: {}'.format(response))
-        response = self._serial.readline().strip()
+        # read first response line:
+        response = self._serial.readline().strip()  # expect: "status 9\n"
+        if not response:  # blank line
+            response = self._serial.readline().strip()
+        if response.startswith('status '):
+            raise CommunicationException('Controller doesnt acknowledge status command, instead got: {}'.format(response))
+        # parse N (number of lines that follow):
         try:
-            self._n = int(response)
+            print("response:")
+            print(response)
+            self._n = int(response[len('status '):])
         except ValueError:
             raise CommunicationException('Expected int, got: {}'.format(response))
-        self._status = []
+        # read remaining reaponse lines:
+        new_status = {}
         for _ in range(self._n):
             response = self._serial.readline().strip()
-            if response in ("ON", "OFF"):
-                self._status.append(response == "ON")
-            else:
-                raise CommunicationException('Expected "ON" or "OFF", got: {}'.format(response))
+            key, _, value = map(str.strip, response.partition(':'))
+            if key not in self._default_status:
+                raise CommunicationException(
+                    'Got unknown status key: {}: {}'.format(key, value))
+            new_status[key] = self._default_status_type[key](value)
+        if len(new_status) != len(self._default_status):
+            raise CommunicationException(
+                'Mismatched status keys:\n'
+                'new_status:\n'
+                '{}\n'
+                'known keys:\n'
+                '{}'
+                .format(new_status.keys(), self._default_status.keys()))
+
+        self._status = new_status
 
     def _switch(self, n, state):
         on_off = 'on' if state else 'off'
@@ -134,6 +181,7 @@ class FocuserController(object):
             raise CommunicationException('Expected "ON" or "OFF", got: {}'.format(response))
         self._status[n - 1] = True if response == 'ON' else False
 
+
 class CommunicationException(Exception):
     """ There was an error communicating with Arduino controller """
     pass
@@ -141,7 +189,7 @@ class CommunicationException(Exception):
 
 def _main(args):
 
-    controller = SwitchController(SERIAL, RESET_DISABLED)
+    controller = FocuserController(SERIAL, RESET_DISABLED)
 
     if args['status']:
         controller.pretty_status()        
@@ -150,6 +198,7 @@ def _main(args):
         controller.switch(args['<N>'], state)
         
     exit(0)
+
 
 if __name__ == '__main__':
     args = docopt(__doc__, version=__doc__.split('\n'[0]), options_first=True)
